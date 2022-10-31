e99d44f8190914a3dd93d9a3908fa42e1641eb8f
zuechai <zuechai@gmail.com>
2022-10-31 13:07:33 -0400

    Build out structure of NavBar and start styling

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
+// import {
+//   createBrowserHistory,
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+
 import "./App.css";
 
-function App() {
-  const recipes = [
-    {
-      id: 1,
-      name: "Fermented Napa Cabbage",
-      yield: "1 quart jar",
-      image: null,
-      ingredients: [
-        {
-          id: 1,
-          name: "napa cabbage",
-          quantity: 1,
-          unit: "head",
-          preparation: "",
-        },
-        {
-          id: 1,
-          name: "napa cabbage",
-          quantity: 1,
-          unit: "head",
-          preparation: "",
-        },
-      ],
-      method: ["do this in step 1", "step 2", "step 3"],
-      notes: "anything else important about the important",
-    },
-  ];
+const recipes = [
+  {
+    id: 1,
+    name: "Fermented Napa Cabbage",
+    yield: "1 quart jar",
+    image: null,
+    ingredients: [
+      {
+        id: 1,
+        name: "napa cabbage",
+        quantity: 1,
+        unit: "head",
+        preparation: "",
+      },
+      {
+        id: 1,
+        name: "napa cabbage",
+        quantity: 1,
+        unit: "head",
+        preparation: "",
+      },
+    ],
+    method: ["do this in step 1", "step 2", "step 3"],
+    notes: "anything else important about the important",
+  },
+];
 
-  return <div>Recipe Index App</div>;
+function App() {
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <RecipePage />,
+  //     children: [],
+  //   },
+  // ]);
+
+  return (
+    <>
+      <NavBar />
+      <div>Recipe Index App</div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+// import { Link } from "react-router-dom";
+
+import "./NavBar.scss";
+
+export default function NavBar() {
+  return (
+    <header className="nb-header">
+      <div className="nb-logo__wrapper">
+        <h1 className="nb-logo__text">Logo</h1>
+      </div>
+      <div className="nb-searchbar">{/* Hidden Search Bar */}</div>
+      <ul className="nb-nav">
+        <li className="nb-nav__item">
+          <a className="nb-nav__link">Index</a>
+        </li>
+        <li className="nb-nav__item">
+          <a className="nb-nav__link">Account</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
