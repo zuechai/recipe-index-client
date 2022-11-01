a64cda01d9a8cdf2d307b50c611cd1872e11a8b5
zuechai <zuechai@gmail.com>
2022-10-31 21:40:19 -0400

    Implement state and useEffect for storing the selected recipe, add pseudocode

--- src/pages/RecipePage/RecipePage.js
+++ src/pages/RecipePage/RecipePage.js
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
 import "./RecipePage.scss";
 
 export default function RecipePage({ recipe }) {
+  const [selectedRecipe, setSelectedRecipe] = useState();
+
+  useEffect(() => {
+    if (!selectedRecipe) {
+      setSelectedRecipe({ ...recipe });
+      console.log("useEffect hit");
+    }
+  }, [recipe, selectedRecipe]);
+
+  if (!selectedRecipe) {
+    return <h2>Loading...</h2>;
+  }
+
   return <h2>{recipe.name}</h2>;
 }
+
+// BodyHeader
+//  title & breadcrumbs
+// RecipeImage if not null
+// IngredientCard
+// Methods
