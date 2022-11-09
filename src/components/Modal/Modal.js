7625bbd579386150ec293efa7a579bb4b1b96b10
zuechai <zuechai@gmail.com>
2022-11-09 00:42:33 -0500

    Implement modal form without endpoint functionality

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,9 @@
+import { useRef } from "react";
 import styles from "./Modal.module.scss";
 
 export default function Modal({ open, setOpen }) {
+  const checkRef = useRef();
+
   if (!open) {
     return null;
   }
@@ -26,7 +29,7 @@ export default function Modal({ open, setOpen }) {
               <input
                 type="text"
                 className={styles.modal__textInput}
-                name="textInput"
+                id="textInput"
                 placeholder="Find Users"
                 onKeyDown={(e) => {
                   if (e.key === "Enter") {
@@ -39,7 +42,7 @@ export default function Modal({ open, setOpen }) {
               <button
                 className={styles.modal__button}
                 onClick={() => {
-                  console.log("clicked");
+                  console.log("find users");
                   setOpen(false);
                 }}
               >
@@ -48,26 +51,53 @@ export default function Modal({ open, setOpen }) {
             </div>
             <div className={styles.modal__divider} />
             <ul className="">
-              <li className="" key={"id"}>
-                <div className={styles.modal__inputWrap}>
-                  <input type="checkbox" name={"id"} value={"id"} />
-                  <label htmlFor={"id"} className="">
-                    User name
-                  </label>
-                </div>
-                <div className={styles.modal__inputWrap}>
-                  <label htmlFor={"canEdit"} className="">
-                    Give access to edit recipe:
-                  </label>
-                  <input type="checkbox" name={"canEdit"} value={"true"} />
-                </div>
-              </li>
+              {
+                // map users here
+                <li className={styles.modal__userItem} key={"id"}>
+                  <div className={styles.modal__userOptions}>
+                    <label htmlFor={"id"} className="">
+                      User name
+                    </label>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className={styles.modal__icon}
+                      onClick={(e) => {
+                        console.log("delete");
+                        setOpen(false);
+                      }}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </div>
+                  <div className={styles.modal__userOptions}>
+                    <input
+                      type="checkbox"
+                      id={"canEdit"}
+                      value="canEdit"
+                      className={styles.modal__checkbox}
+                      ref={checkRef}
+                      onChange={(e) => console.log(checkRef.current.checked)}
+                    />
+                    <label htmlFor={"canEdit"} className="">
+                      Give access to edit recipe
+                    </label>
+                  </div>
+                </li>
+              }
             </ul>
             <div className={styles.modal__inputWrap}>
               <button
-                className={styles.modal__button}
+                className={`${styles.modal__button} ${styles.modal__buttonSubmit}`}
                 onClick={() => {
-                  console.log("clicked");
+                  console.log("submit");
                   setOpen(false);
                 }}
               >
