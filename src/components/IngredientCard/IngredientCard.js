fc07b0db5d1a7bcbd41d909758f99a0024b89484
zuechai <zuechai@gmail.com>
2022-11-09 21:24:23 -0500

    Fix bugs for new recipes

--- src/components/IngredientCard/IngredientCard.js
+++ src/components/IngredientCard/IngredientCard.js
@@ -1,6 +1,25 @@
 import styles from "./IngredientCard.module.scss";
 
 export default function IngredientCard({ ingredients }) {
+  const tableRows = ingredients.map(
+    ({ ingredientId, measurement, ingredient }) => {
+      let quantity,
+        unit = null;
+      if (measurement !== null) {
+        [quantity, unit] = measurement.split(" ");
+      } else {
+      }
+
+      return (
+        <tr key={ingredientId} className={styles.ingredientList__item}>
+          <td className={styles.ingredientList__quantity}>{quantity}</td>
+          <td className={styles.ingredientList__unit}>{unit}</td>
+          <td className={styles.ingredientList__ingredient}>{ingredient}</td>
+        </tr>
+      );
+    }
+  );
+
   return (
     <section className={styles.section}>
       <article className={styles.card}>
@@ -8,23 +27,7 @@ export default function IngredientCard({ ingredients }) {
           <h3 className={styles.card__headerText}>Ingredients</h3>
         </div>
         <table className={styles.ingredientList}>
-          <tbody>
-            {ingredients.map(({ ingredientId, measurement, ingredient }) => {
-              const [quantity, unit] = measurement.split(" ");
-              console.log(quantity, unit);
-              return (
-                <tr key={ingredientId} className={styles.ingredientList__item}>
-                  <td className={styles.ingredientList__quantity}>
-                    {quantity}
-                  </td>
-                  <td className={styles.ingredientList__unit}>{unit}</td>
-                  <td className={styles.ingredientList__ingredient}>
-                    {ingredient}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
+          <tbody>{tableRows}</tbody>
         </table>
       </article>
     </section>
