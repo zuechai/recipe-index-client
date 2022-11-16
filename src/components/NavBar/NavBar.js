b4e72d277edfd67badf8b45c4da9899eb32d129d
zuechai <zuechai@gmail.com>
2022-11-16 16:06:36 -0500

    Implement a container to set a max-width for the content, repeat on remaining component containers

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,22 +5,24 @@ import styles from "./NavBar.module.scss";
 export default function NavBar() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo__wrapper}>
-        <h1 className={styles.logo__text}>Rcp Indx</h1>
+      <div className={styles.container}>
+        <div className={styles.logo__wrapper}>
+          <h1 className={styles.logo__text}>Rcp Indx</h1>
+        </div>
+        <div className={styles.searchbar}>{/* Hidden Search Bar */}</div>
+        <ul className={styles.nav}>
+          <li className={styles.nav__item}>
+            <Link to="/" className={styles.nav__link}>
+              Index
+            </Link>
+          </li>
+          <li className={styles.nav__item}>
+            <Link to="/" className={styles.nav__link}>
+              Account
+            </Link>
+          </li>
+        </ul>
       </div>
-      <div className={styles.searchbar}>{/* Hidden Search Bar */}</div>
-      <ul className={styles.nav}>
-        <li className={styles.nav__item}>
-          <Link to="/" className={styles.nav__link}>
-            Index
-          </Link>
-        </li>
-        <li className={styles.nav__item}>
-          <Link to="/" className={styles.nav__link}>
-            Account
-          </Link>
-        </li>
-      </ul>
     </header>
   );
 }
